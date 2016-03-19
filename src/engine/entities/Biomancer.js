dca13d6fa3877d2949bb7110de54df31a3d57cb6
Kyle Bibler <kyle.bibler08@gmail.com>
2016-03-19 19:31:37 -0400

    Took rotation computations out of game loop
    Also added speed variable

--- src/engine/entities/Biomancer.js
+++ src/engine/entities/Biomancer.js
@@ -4,7 +4,18 @@ var BIOMANCER_VARS = {
 	ID: "player",
 	HEALTH: 100,
 	FILENAME: "biomancer/main-char/biomancer.png",
-	GUN_POSITION: {x: 45, y: 52}
+	GUN_POSITION: {x: 45, y: 52},
+	SPEED: 3
+};
+var ROTATION = {
+	S: 0,
+	SW: Math.PI/4,
+	W: Math.PI/2,
+	NW: 3 * Math.PI/4,
+	N: Math.PI,
+	NE: 5 * Math.PI/4,
+	E: 3 * Math.PI/2,
+	SE: 7 * Math.PI/4
 };
 
 
@@ -33,22 +44,22 @@ class Biomancer extends Entity {
 
 		// Left
 		if (pressedKeys.contains(37)) {
-			this.addToMovement(-1, 0);
+			this.addToMovement(-BIOMANCER_VARS.SPEED, 0);
 		}
 
 		// Up
 		if (pressedKeys.contains(38)) {
-			this.addToMovement(0, -1);
+			this.addToMovement(0, -BIOMANCER_VARS.SPEED);
 		}
 
 		// Right
 		if (pressedKeys.contains(39)) {
-			this.addToMovement(1, 0);
+			this.addToMovement(BIOMANCER_VARS.SPEED, 0);
 		}
 
 		// Down
 		if (pressedKeys.contains(40)) {
-			this.addToMovement(0, 1);
+			this.addToMovement(0, BIOMANCER_VARS.SPEED);
 		}
 
 		// Set Rotation
@@ -64,28 +75,28 @@ class Biomancer extends Entity {
 
 		if (pressedKeys.contains(37) && pressedKeys.contains(38)) {
 			// North-west
-			this.rotation = 3 * Math.PI / 4;
+			this.rotation = ROTATION.NW;
 		} else if (pressedKeys.contains(38) && pressedKeys.contains(39)) {
 			// North-east
-			this.rotation = 5 * Math.PI / 4;
+			this.rotation = ROTATION.NE;
 		} else if (pressedKeys.contains(39) && pressedKeys.contains(40)) {
 			// South-east
-			this.rotation = 7 * Math.PI / 4;
+			this.rotation = ROTATION.SE;
 		} else if (pressedKeys.contains(40) && pressedKeys.contains(37)) {
 			// South-west
-			this.rotation = Math.PI / 4;
+			this.rotation = ROTATION.SW;
 		} else if (pressedKeys.contains(37)) {
 			// West
-			this.rotation = Math.PI / 2;
+			this.rotation = ROTATION.W;
 		} else if (pressedKeys.contains(38)) {
 			// North
-			this.rotation = Math.PI;
+			this.rotation = ROTATION.N;
 		} else if (pressedKeys.contains(39)) {
 			// East
-			this.rotation = 3 * Math.PI / 2;
+			this.rotation = ROTATION.E;
 		} else if (pressedKeys.contains(40)) {
 			// South
-			this.rotation = 0;
+			this.rotation = ROTATION.S;
 		}
 	}
 
