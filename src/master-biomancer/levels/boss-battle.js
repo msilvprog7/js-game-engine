7722719bb2ad7224f73a4457bb7d031c5d802828
jtr4fh <jtr4fh@virginia.edu>
2016-05-01 20:24:00 -0400

    fixed bug with boss battle

--- src/master-biomancer/levels/boss-battle.js
+++ src/master-biomancer/levels/boss-battle.js
@@ -3,5 +3,7 @@
 var BOSSBATTLE = 
 {
 	id: "boss-battle",
-	level: "Tiles0 1 3 position 0 0 | Tiles0 3 1 position -400 -400 | Tiles0 3 1 position -400 -800 | Tiles0 3 1 position -400 -1200 | Wall0-Tall 3 position -50 0 | Wall0 position -50 1200 | Wall0 position 400 1200 | Wall0-Wide 1 position 0 1200 | Wall0-Tall 3 position 400 0 | Wall0-Wide 1 position 400 0 | Wall0 position 800 0 | Wall0-Tall 3 position 800 -1200 | Wall0 position 800 -1250 | Wall0 position -450 -1250 | Wall0-Wide 3 position -400 -1250 | Wall0-Tall 3 position -450 -1200 | Wall0 position -450 0 | Wall0-Wide 1 position -400 0 | Biomancer position 176 1050 |SealedWall 8 position 0 -75 | "
+	level: "Tiles0 1 3 position 0 0 | Tiles0 3 1 position -400 -400 | Tiles0 3 1 position -400 -800 | Tiles0 3 1 position -400 -1200 | Wall0-Tall 3 position -50 0 | Wall0 position -50 1200 | Wall0 position 400 1200 | Wall0-Wide 1 position 0 1200 | Wall0-Tall 3 position 400 0 | Wall0-Wide 1 position 400 0 | Wall0 position 800 0 | Wall0-Tall 3 position 800 -1200 | Wall0 position 800 -1250 | Wall0 position -450 -1250 | Wall0-Wide 3 position -400 -1250 | Wall0-Tall 3 position -450 -1200 | Wall0 position -450 0 | Wall0-Wide 1 position -400 0 |SealedWall 8 position 0 -75 | Biomancer position 176 1050 |",
+	tl: {x: -450, y: -1250},
+	br: {x: 850, y: 1250}
 };
